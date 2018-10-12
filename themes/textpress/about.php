c0427051dc59c6c61005fd2b975656328584662a
DESKTOP-2GM2KOK\Luis Enrique <piracarter@hotmail.com>
2018-10-12 10:56:31 -0500

    About primer revision

--- themes/textpress/about.php
+++ themes/textpress/about.php
@@ -1,20 +1,34 @@
-  <h3>About</h3>
-  <p>Hi There,</p>
-  <p>I am Shameer, a developer from God's own country (Kerala), India. I am mainly working on web development using PHP, Scala, Javascript, etc. 
-    Apart from that I do enjoy coding in Ruby (infact, I love it), system administration, database administration, etc. 
-    Occasionally I write about PHP stuffs in <a href="http://www.sitepoint.com/author/shameerc/">SitePoint</a> and about Cloud in <a href="https://www.simple-talk.com/author/shameer/">Simple-Talk</a>.</p>
-  <h3>Philosophy</h3>
-  <p>Though there are many flat-file blog engines, like Toto, Jekyll, etc. I was unable to find a decent one for PHP lovers. 
-    As there are some good PHP Cloud platform services like AppFog and Pagodabox which offers free hosting in cloud, 
-    having a flat-file blog engine written in PHP makes perfect sense. An important reason is it's performance. 
-    Flat file blog engine will load much faster compared to database driven CMS websites. 
-    Also it is highly secure and agile. So I decided not to wait for someone else to do it and here's mine - TextPress. My goal is to provide almost
-    all the necessary features for a blogging engine with TextPress.
-  </p>
-  <strong>Disclaimer</strong> <br>
-    I won't say TextPress is a completely secure and bulletproof application and It's not a big deal to write something like this. 
-    So if you are not comfortable with it, feel happy to fork it, add features and let me know what you have done. 
-    I will always try to improve it and add more features. 
-  <h3>Contact</h3>
-  <p>You can contact me in twitter <a href="http://twitter.com/shameerc">@shameerc</a>. Visit my personal blog <a href="http://blog.shameerc.com">Shameerc.com</a></p>
+  <h3>Acerca de nosotros</h3>
+  <p>Hola,</p>
+  <p>Somos una empresa Mexicana, con profesionales en Tecnolog&iacute;as de la informaci&oacute;n con m&aacute;s de 20 a&ntilde;os de experiencia en la industria, 
+  y profundos entusiastas y evangelistas de las Tecnolog&iacute;as OpenSource, actualmente colaboramos dando servicios de consultor&iacute;a a empresas financieras, 
+  en sus departamentos de TI. &nbsp;En esta constante tan vertiginosa del aprendizaje de nuevas tecnolog&iacute;as, las empresas requieren profesionales mayormente 
+  capacitados y que est&eacute;n preparados para implementar, gestionar y soportar grandes sistemas de informaci&oacute;n escalables, hoy en d&iacute;a la 
+  Capacitaci&oacute;n y la Certificaci&oacute;n permiten llevar al m&aacute;ximo las habilidades y las inversiones en tecnolog&iacute;a.&nbsp; La Certificaci&oacute;n 
+  y Capacitaci&oacute;n pueden lograr un fuerte impacto empresarial. En pa&iacute;ses como M&eacute;xico y Estados Unidos los Ejecutivos de los departamentos tienen 
+  altos niveles de confianza en el personal certificado.</p>
+  <h3>Las certificaciones</h3>
+<p>A trav&eacute;s de la experiencia personal, que ha tenido nuestro equipo al paso de los a&ntilde;os, los ex&aacute;menes de certificaci&oacute;n que ofrecen 
+diferentes compa&ntilde;&iacute;as son complejos:&nbsp; algunos&nbsp; de preguntas de opci&oacute;n m&uacute;ltiple, opci&oacute;n simple, ejercicios y algunos 
+m&aacute;s son totalmente pr&aacute;cticos, cualquiera de estos ex&aacute;menes tiene cierto grado de complejidad y requiere de una preparaci&oacute;n que en algunos 
+casos se proporciona en un centro de Capacitaci&oacute;n autorizado con duraci&oacute;n de 30 a 40 horas, y en la cual se cubre una serie de temas con pr&aacute;cticas. 
+Adicionalmente el candidato a certificarse tendr&aacute; que dedicar&nbsp; durante alg&uacute;n tiempo pr&aacute;ctica en laboratorios para reforzar, adem&aacute;s en 
+algunos casos&nbsp; cuando se trata de ex&aacute;menes de preguntas, los simuladores pueden ser una buena opci&oacute;n. Todo lo anterior contempla una inversi&oacute;n 
+en tiempo y recursos.</p>
+  <h3>Club de Estudio</h3>
+<p>Los servicios que ofrecemos contemplan no solamente un curso, sino el desarrollo de un plan de estudio personal con el cual una persona tiene mejores herramientas 
+y mayores probabilidades de afrontar con &eacute;xito un examen de certificaci&oacute;n en su primer intento.&nbsp; Los elementos incluidos dentro de una de nuestras 
+capacitaciones es lo siguiente:</p>
+<ul>
+<li style="margin-left: 38.25pt; text-indent: -18.0pt;">Desarrollo del plan personal de estudio</li>
+<li style="margin-left: 38.25pt; text-indent: -18.0pt;">Revisi&oacute;n y Comprensi&oacute;n del tipo de examen a presentar.</li>
+<li style="margin-left: 38.25pt; text-indent: -18.0pt;">Orientaci&oacute;n sobre las trayectorias de plan de carrera.</li>
+<li style="margin-left: 38.25pt; text-indent: -18.0pt;">Revisi&oacute;n de los temas/objetivos de examen publicados por la Compa&ntilde;&iacute;a que ofrece el examen.</li>
+<li style="margin-left: 38.25pt; text-indent: -18.0pt;">Implementaci&oacute;n de un laboratorio de estudio para practicar los objetivos del examen.</li>
+<li style="margin-left: 38.25pt; text-indent: -18.0pt;">Laboratorios online para practicar los objetivos del examen.</li>
+<li style="margin-left: 38.25pt; text-indent: -18.0pt;">Material Impreso y kit de alumno</li>
+<li style="margin-left: 38.25pt; text-indent: -18.0pt;">Soporte de 1 mes adicional al t&eacute;rmino del curso en laboratorios online y asesor&iacute;a.</li>
+</ul>
+  <h3>Contacto</h3>
+  <p>Puedes contactarnos a traves de Twitter: <a href="https://twitter.com/certificatestu1">@certificatestu1</a>.</p>
 
